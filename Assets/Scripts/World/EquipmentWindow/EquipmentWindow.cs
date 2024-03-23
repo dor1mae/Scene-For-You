82dd11bdd25cf167c51ede7da05af3f120c179e5
dor1mae <dorima.bop@gmail.com>
2024-03-23 15:57:15 +0700

    Создание анимации открытия и закрытия окна экипировки

--- Assets/Scripts/World/EquipmentWindow/EquipmentWindow.cs
+++ Assets/Scripts/World/EquipmentWindow/EquipmentWindow.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -28,20 +29,48 @@ public class EquipmentWindow : InitClass
         _inventory.Init();
         _searchController = new ItemSearchController(_inventory);
         _presentController.SetItemPresentController(_containerUI, _itemPrefab, _searchController, _controller, _useButton);
+        _animator = GetComponent<Animator>();
 
         OnSearch += _presentController.PresentItems;
         _presentController.PresentItems();
         Debug.Log($"{GetType()}: is initialized");
     }
 
-    private void OnOpenEquipmentWindow()
+    private void Update()
     {
-
+        if(Input.GetKeyDown(KeyCode.I) && !_animator.GetBool("isOpen"))
+        {
+            StartCoroutine(OnOpenEquipmentWindow());
+        }
+        else if(Input.GetKeyDown(KeyCode.I) && _animator.GetBool("isOpen"))
+        {
+            StartCoroutine(OnCloseEquipmentWindow());
+        }
     }
 
-    private void OnCloseEquipmentWindow()
+    private IEnumerator OnOpenEquipmentWindow()
     {
+        gameObject.GetComponent<CanvasGroup>().alpha = 1.0f;
 
+        _animator.SetBool("isOpen", true);
+
+        _animator.Play("Open");
+
+        Debug.Log("Попытка вызвать открытие и включение объекта");
+        yield return new WaitForSeconds(1);
+    }
+
+    private IEnumerator OnCloseEquipmentWindow()
+    {
+        _animator.SetBool("isOpen", false);
+        
+        _animator.Play("Close");
+
+        yield return new WaitForSeconds(1);
+
+        Debug.Log("Попытка вызвать закрытие и отключение объекта");
+         
+        gameObject.GetComponent<CanvasGroup>().alpha = 0.0f;
     }
 }
 
